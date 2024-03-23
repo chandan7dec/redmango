6ba643997a16c23297f68d1d149bee0db6cd4583
chandan7dec <ck.kislay@gmail.com>
2024-03-23 15:14:54 +0530

    Completed section 17: Setup Redux and RTK

--- src/Pages/MenuItemDetails.tsx
+++ src/Pages/MenuItemDetails.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useGetMenuItemByIdQuery } from '../Apis/menuItemApi';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function MenuItemDetails() {
+  const {menuItemId} = useParams();
+  const {data,isLoading} = useGetMenuItemByIdQuery(menuItemId);
+  const navigate = useNavigate();
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantity = (counter:number)=> {
+    let newQuantity = quantity + counter;
+    if(newQuantity == 0) {
+      newQuantity = 1;
+    }
+    setQuantity(newQuantity);
+    return;
+  }
+
   return (
-    <div className="container pt-4 pt-md-5">
-    <div className="row">
+    
+  <div className="container pt-4 pt-md-5">
+  {!isLoading ? (<div className="row">
       <div className="col-7">
-        <h2 className="text-success">NAME</h2>
+        <h2 className="text-success">{data.result?.name}</h2>
         <span>
           <span
             className="badge text-bg-dark pt-2"
             style={{ height: "40px", fontSize: "20px" }}
           >
-            CATEGORY
+            {data.result?.category}
           </span>
         </span>
         <span>
@@ -19,25 +38,29 @@ function MenuItemDetails() {
             className="badge text-bg-light pt-2"
             style={{ height: "40px", fontSize: "20px" }}
           >
-            SPECIAL TAG
+            {data.result?.specialTag}
           </span>
         </span>
         <p style={{ fontSize: "20px" }} className="pt-2">
-          DESCRIPTION
+          {data.result?.description}
         </p>
-        <span className="h3">$10</span> &nbsp;&nbsp;&nbsp;
+        <span className="h3">${data.result?.price}</span> &nbsp;&nbsp;&nbsp;
         <span
           className="pb-2  p-3"
           style={{ border: "1px solid #333", borderRadius: "30px" }}
         >
           <i
             className="bi bi-dash p-1"
-            style={{ fontSize: "25px", cursor: "pointer" }}
+            style={{ fontSize: "25px", cursor: "pointer" }} onClick={ () =>{
+              handleQuantity(-1);
+            }}
           ></i>
-          <span className="h3 mt-3 px-3">XX</span>
+          <span className="h3 mt-3 px-3">{quantity}</span>
           <i
             className="bi bi-plus p-1"
-            style={{ fontSize: "25px", cursor: "pointer" }}
+            style={{ fontSize: "25px", cursor: "pointer" }} onClick={ () =>{
+              handleQuantity(+1);
+            }}
           ></i>
         </span>
         <div className="row pt-4">
@@ -48,7 +71,8 @@ function MenuItemDetails() {
           </div>
 
           <div className="col-5 ">
-            <button className="btn btn-secondary form-control">
+            <button className="btn btn-secondary form-control" onClick={() =>navigate(-1)}>
+
               Back to Home
             </button>
           </div>
@@ -62,7 +86,14 @@ function MenuItemDetails() {
           alt="No content"
         ></img>
       </div>
+    </div>)
+  :(
+    <div className='d-flex justify-content-center ' style={{width:"100%"}}>
+     <div>Loading....</div>
     </div>
+  )}
+
+    
   </div>
   )
 }
